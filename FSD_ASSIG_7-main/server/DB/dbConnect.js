b91f21ba1fdb68cfbee65a4e10dd5b6517ad318a
chanakya9999 <nalluriprudhvichanakya@gmail.com>
2024-11-08 19:07:31 +0530

    updated

--- FSD_ASSIG_7-main/server/DB/dbConnect.js
+++ FSD_ASSIG_7-main/server/DB/dbConnect.js
@@ -1,22 +1,18 @@
 
 const mongoose = require("mongoose");
-require('dotenv').config({ path: 'E:\react\-FSD_ASSIG_7\FSD_ASSIG_7-main\.env' });
+require('dotenv').config({ path: '.\.env' });
 
 async function dbConnect(){
 
-    mongoose.connect(process.env.DB_URL,
-        { useNewUrlParser: true, 
-            useUnifiedTopology: true, 
-            writeConcern: { w: 'majority', j: true, wtimeout: 1000 } 
-        })
-    .then(()=>{
-        console.log("Successfully connected to MongoDB Atlas");
-    })
-    .catch((error) => {
-        console.log("Unable to connect to the MongoDB Atlas!");
-        console.error(error);
-    })
+
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Could not connect to MongoDB", err));
 }
 
 module.exports = dbConnect;
+
+
+  
+  
 
